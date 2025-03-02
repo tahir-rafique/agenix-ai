75cfe39cc28849e7a6c14d3f0b9329b001eb3261
itstahirrafique <hello.tahirrafique@gmail.com>
2025-03-02 14:15:24 +0500

    navbar mobile height issue updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
       </div>
 
       {menuOpen && (
-        <div className="bg-muted py-4 w-[300px] fixed top-0 z-50  left-0 lg:hidden flex flex-col gap-4 px-6 transition-all duration-300 ease-linear h-full">
+        <div className="bg-muted py-4 w-[300px] fixed top-0 bottom-0 z-50  left-0 lg:hidden flex flex-col gap-4 px-6 transition-all duration-300 ease-linear h-full">
           <div>
             <a href="/">
               <img src={logo} alt="" className="h-20 max-sm:h-14" />
